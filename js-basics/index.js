a36ea1f8c8b936c28b88a5c1b93856848da7a5e8
Jerel Miller <jerelmiller@gmail.com>
2017-10-04 00:12:14 -0700

    Add more var examples

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,5 +1,71 @@
-// var, const, let
+/**
+ * var, let, const
+ * --------------
+ * Learn the basics of variable declaration
+ */
 
+// ----- var -----
 var myVar = 'Not a clever variable name'
 
 console.log(myVar)
+
+// -------
+function teachFunctionScope() {
+  var myVar = 'Still a terrible variable name'
+
+  console.log(myVar)
+}
+
+teachFunctionScope()
+console.log(myVar)
+
+// --------
+function teachVarReassignment() {
+  var firstName = 'Jerel'
+  firstName = 'Bob'
+
+  console.log(firstName)
+}
+
+teachVarReassignment()
+
+// -----------
+function teachVarRedeclaration() {
+  var firstName = 'Jerel'
+  var firstName = 'Bob'
+
+  console.log(firstName)
+}
+
+teachVarRedeclaration()
+
+// ------------
+function teachVarHoisting() {
+  console.log(firstName)
+  var firstName = 'Jerel'
+
+  console.log(firstName)
+}
+
+teachVarHoisting()
+
+// -----------
+function teachVarBlockScope() {
+  var age = 22
+  var legal = false
+
+  if (age > 10) {
+    var dogYears = age * 7
+    console.log(dogYears)
+  }
+
+  console.log(dogYears)
+
+  if (age >= 21) {
+    var legal = true
+  }
+
+  console.log(legal)
+}
+
+teachVarBlockScope()
