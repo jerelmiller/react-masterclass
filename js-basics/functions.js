ea6e205674c5402eca5fd8217f5c5bbce7854c2f
Jerel Miller <jerelmiller@gmail.com>
2017-10-04 01:06:41 -0700

    Create an intermediate-js folder and move callback lesson over theregs

--- js-basics/functions.js
+++ js-basics/functions.js
@@ -35,54 +35,6 @@ myAnonymousFunction()
 
 /* -------------------------- */
 
-const doSomethingWithOne = function(callback) {
-  callback(1)
-}
-
-doSomethingWithOne(function(value) {
-  console.log(`The value is ${value}`)
-})
-
-/* -------------------------- */
-
-const giveMeSomethingRidiculous = function(callback) {
-  const items = [
-    'solar powered cooling hat',
-    'alien butler',
-    'wrist cell phone carrier'
-  ]
-  const randomItem = items[Math.floor(Math.random() * items.length)]
-
-  callback(randomItem)
-}
-
-const handleRidiculousItem = function(item) {
-  console.log(`Get that ${item} away from me`)
-}
-
-giveMeSomethingRidiculous(handleRidiculousItem)
-
-/* -------------------------- */
-
-// implement map
-
-const map = function(array, callback) {
-  const newArray = []
-  for (let i = 0; i < array.length; i++) {
-    newArray.push(callback(array[i]))
-  }
-  return newArray
-}
-
-const values = [1, 2, 3]
-const doubled = map(values, function(num) {
-  return num * 2
-})
-
-console.log(doubled)
-
-/* -------------------------- */
-
 const sayings = {
   hello: function() {
     console.log('You dawg, whats the haps?')
