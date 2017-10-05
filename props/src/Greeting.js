cbfa66804ab5f0efeb2f2c2753880d28272a0287
Jerel Miller <jerelmiller@gmail.com>
2017-10-04 23:13:18 -0700

    Use function syntax for Greeting component

--- props/src/Greeting.js
+++ props/src/Greeting.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-export default class Greeting extends Component {
-  render() {
-    return <h1>Hello { this.props.name }</h1>
-  }
+export default function Greeting(props) {
+  return <h1>Hello { props.name }</h1>
 }
