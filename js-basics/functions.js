3f97e8c588eb1a65ebe0054b0279a2a99af99c63
Jerel Miller <jerelmiller@gmail.com>
2017-10-05 00:52:53 -0700

    Add notes to first-half of functions

--- js-basics/functions.js
+++ js-basics/functions.js
@@ -1,4 +1,7 @@
-/* -------------------------- */
+/* ------ Declaring functions ------ */
+
+// To declare a new function, use the `function` keyword. This creates a named
+// function that you can call later.
 
 function myFirstFunction() {
   console.log('yippeeee')
@@ -6,7 +9,12 @@ function myFirstFunction() {
 
 myFirstFunction()
 
-/* -------------------------- */
+/* ------ Function arguments ------ */
+
+// Unlike most languages, JavaScript does not care if you give a function a set
+// of arguments that does not match its arity. When you give less arguments,
+// the remaining arguments will be given a value of `undefined`. When you give
+// more arguments, the others are simply ignored.
 
 function withArgs(firstName, lastName) {
   console.log(firstName, lastName)
@@ -16,8 +24,27 @@ withArgs('Jim', 'Halpert')
 withArgs('Pam')
 withArgs('Bob', 'Vance', 'Vance Refridgeration')
 
+/* ------ Anonymous functions ------ */
 
-/* -------------------------- */
+// JavaScript also allows you to create anonymous functions. These are
+// functions without a name. Functions are also first class citizens, meaning
+// you can treat them as any other variable. This allows you to pass them
+// around like you would any other value. This allows you to assign anonymous
+// functions to variables, which can then be used just like any other function.
+
+const myAnonymousFunction = function() {
+  console.log('I work the same as a regular function')
+}
+
+const copiedAnonymousFunction = myAnonymousFunction
+
+myAnonymousFunction()
+copiedAnonymousFunction()
+
+/* ------ Hoisting ------ */
+
+// Much like `var`, functions are hoisted. This means that you can use them
+// before they are declared. This only works with named functions.
 
 myHoistedFunction()
 
@@ -25,15 +52,10 @@ function myHoistedFunction() {
   console.log("I can get called even before I'm declared")
 }
 
-/* -------------------------- */
+/* ------ Functions as values within objects ------ */
 
-const myAnonymousFunction = function() {
-  console.log('I work the same as a regular function')
-}
-
-myAnonymousFunction()
-
-/* -------------------------- */
+// As mentioned, JavaScript functions are first-class citizens. This means that
+// you can assign functions as values within objects.
 
 const sayings = {
   hello: function() {
@@ -47,23 +69,31 @@ const sayings = {
 sayings.hello()
 sayings.goodbye()
 
-/* -------------------------- */
+/* ------ ES6 shorthand ------ */
 
-const dishes = {
+// ES6 (a.k.a. ES2015) introduced a shorthand for declaring functions as values
+// within objects. This allows you to omit the `function` keyword and colon.
+
+const meals = {
   breakfast() {
     console.log("Too bad I'm not a chef")
   },
   lunch() {
     console.log("Ya, I can't get that for you")
   },
-  dinner() {
-    console.log("You demand too much")
+  dinner(withDessert) {
+    if (withDessert) {
+      console.log('You demand too much')
+    } else {
+      console.log('You best be full')
+    }
   }
 }
 
-dishes.breakfast()
-dishes.lunch()
-dishes.dinner()
+meals.breakfast()
+meals.lunch()
+meals.dinner(true)
+meals.dinner(false)
 
 /* -------------------------- */
 
