64b011374299262a95f65f970ff4c122e8050f61
Jerel Miller <jerelmiller@gmail.com>
2017-10-04 23:10:00 -0700

    Move Hello into own component file and render element directly

--- props/src/index.js
+++ props/src/index.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-class Hello extends Component {
-  render() {
-    return <h1>Hello</h1>
-  }
-}
-
-ReactDOM.render(<Hello />, document.getElementById('root'))
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'))
