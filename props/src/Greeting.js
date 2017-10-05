a861a67eae2ee73840ca2176cfd04a8ebd362f3b
Jerel Miller <jerelmiller@gmail.com>
2017-10-05 01:23:34 -0700

    Update Greeting to use arrow function

--- props/src/Greeting.js
+++ props/src/Greeting.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
-export default function Greeting(props) {
-  return <h1>Welcome to React Masterclass, { props.name }</h1>
-}
+const Greeting = props => (
+  <h1>Welcome to React Masterclass, { props.name }</h1>
+)
+
+export default Greeting
