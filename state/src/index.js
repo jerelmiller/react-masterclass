1e5c9362844b1ef690387bafb2da692362c9471e
Jerel Miller <jerelmiller@gmail.com>
2017-10-11 23:43:58 -0700

    Change initial rendered element

--- state/src/index.js
+++ state/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <h1>Import a component to get started</h1>,
+  document.getElementById('root')
+)
