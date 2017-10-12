020b60b6345e9378d6cf714aca09086b54b04544
Jerel Miller <jerelmiller@gmail.com>
2017-10-11 22:31:06 -0700

    Move classes to js-basics folder and add some descriptions

--- js-basics/classes.js
+++ js-basics/classes.js
@@ -1,3 +1,7 @@
+/* ------ Declaring classes ------ */
+
+// To declare a class, use the `class` keyword followed by the identifier.
+
 class Animal {
   constructor(noise) {
     this.noise = noise
@@ -11,7 +15,10 @@ class Animal {
 const barker = new Animal('woof')
 barker.makeNoise()
 
-/* -------------------------------- */
+/* ------ Extending other classes ------ */
+
+// JavaScript allows you to inherit from other classes. To do so, use
+// the `extends` keyword.
 
 class Cat extends Animal {
   constructor() {
